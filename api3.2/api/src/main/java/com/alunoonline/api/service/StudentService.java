b2c8f7e354fbfc1d2a47bd463598a478934b3ccc
Danielpqueiroz <112348645+Danielpqueiroz@users.noreply.github.com>
2023-11-28 14:44:46 -0300

    atualização service

--- api3.2/api/src/main/java/com/alunoonline/api/service/StudentService.java
+++ api3.2/api/src/main/java/com/alunoonline/api/service/StudentService.java
@@ -3,7 +3,9 @@ package com.alunoonline.api.service;
 import com.alunoonline.api.model.Student;
 import com.alunoonline.api.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -27,5 +29,24 @@ public class StudentService {
 
     public void delete(Long id) {
         repository.deleteById(id);
+    }
+
+    public void atualizar(Long id, Student alunoAtualizado) {
+        Optional<Student> alunoExistente = repository.findById(id);
+
+        if (alunoExistente.isPresent()) {
+
+            Student alunoParaAtualizar = alunoExistente.get();
+            alunoParaAtualizar.setNome(alunoAtualizado.getNome());
+            alunoParaAtualizar.setEmail(alunoAtualizado.getEmail());
+            alunoParaAtualizar.setCurso(alunoAtualizado.getCurso());
+
+
+            repository.save(alunoParaAtualizar);
+        } else {
+            
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Aluno não encontrado com o ID: " + id);
+
+        }
     }
 }
