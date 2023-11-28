719f4044b09cd49457d257fa9d39b7040cf46cbd
Danielpqueiroz <112348645+Danielpqueiroz@users.noreply.github.com>
2023-11-28 14:41:43 -0300

    atualização repoistory

--- api3.2/api/src/main/java/com/alunoonline/api/repository/StudentRepository.java
+++ api3.2/api/src/main/java/com/alunoonline/api/repository/StudentRepository.java
@@ -2,9 +2,18 @@ package com.alunoonline.api.repository;
 
 import com.alunoonline.api.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
+    @Modifying
+    @Query("UPDATE ALUNO a SET a.nome = :nome, a.email = :email, a.curso =curso WHERE a.id = :id")
+    void update(@Param("id") Long id, @Param("nome") String nome, @Param("email") String email, @Param("curso") String curso);
+
 }
+
+
